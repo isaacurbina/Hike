252913a142c8b2672f5a8d9c5b04ceb2e42cf1df
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-02-15 13:27:53 -0600

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Isaac Urbina on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
