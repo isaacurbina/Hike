ab61d9c17e463c676c4b958720718c5c5707cca8
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-02-17 17:22:27 -0600

    29. 3. Card Header View

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -12,23 +12,63 @@ struct CardView: View {
 		ZStack {
 			CustomBackgroundView()
 			
-			ZStack {
-				Circle()
-					.fill(
-						LinearGradient(
-							colors: [
-								Color("ColorIndigoMedium"),
-								Color("ColorSalmonLight")
-							],
-							startPoint: .topLeading,
-							endPoint: UnitPoint.bottomTrailing
+			VStack(alignment: .leading) {
+				// MARK: - header
+				VStack(alignment: .leading) {
+					HStack {
+						Text("Hiking")
+							.fontWeight(.black)
+							.font(.system(size: 52))
+							.foregroundStyle(
+								LinearGradient(
+									colors: [
+										.customGrayLight,
+										.customGrayMedium
+									],
+									startPoint: .top,
+									endPoint: .bottom
+								)
+							)
+						
+						Spacer()
+						
+						Button {
+							// ACTION: Show a Sheet
+							print("The button was pressed.")
+						} label: {
+							CustomButtonView()
+						}
+					}
+					
+					Text("Fun and enjoyable outdoor activity for friends and families.")
+						.multilineTextAlignment(.leading)
+						.italic()
+						.foregroundColor(.customGrayMedium)
+				}
+				.padding(.horizontal, 30)
+				
+				// MARK: - main content
+				
+				ZStack {
+					Circle()
+						.fill(
+							LinearGradient(
+								colors: [
+									Color(.colorIndigoMedium),
+									Color(.colorSalmonLight)
+								],
+								startPoint: .topLeading,
+								endPoint: UnitPoint.bottomTrailing
+							)
 						)
-					)
-					.frame(width: 256, height: 256)
+						.frame(width: 256, height: 256)
+					Image("image-1")
+						.resizable()
+						.scaledToFit()
+				}
+				
+				// MARK: - footer
 			}
-			Image("image-1")
-				.resizable()
-				.scaledToFit()
 		}
 		.frame(width: 320, height: 570)
 	}
