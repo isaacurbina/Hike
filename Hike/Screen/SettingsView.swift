3ce2009106e7785286144a49aec34f6de63ea7fa
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-02-19 19:13:20 -0600

    34. 8. Alternate App Icons

--- Hike/Screen/SettingsView.swift
+++ Hike/Screen/SettingsView.swift
@@ -8,6 +8,19 @@
 import SwiftUI
 
 struct SettingsView: View {
+	
+	
+	// MARK: - properties
+	
+	private let alternateAppIcons: [String] = [
+		"AppIcon-MagnifyingGlass",
+		"AppIcon-Map",
+		"AppIcon-Mushroom",
+		"AppIcon-Camera",
+		"AppIcon-Backpack",
+		"AppIcon-Campfire"
+	]
+	
 	var body: some View {
 		
 		List {
@@ -63,6 +76,50 @@ struct SettingsView: View {
 			
 			// MARK: - section icons
 			
+			Section(
+				header: Text("Alternate Icons")
+			) {
+				ScrollView(.horizontal, showsIndicators: false) {
+					
+					HStack(spacing: 12) {
+						
+						ForEach(alternateAppIcons.indices, id: \.self) { item in
+							let appIcon = alternateAppIcons[item]
+							
+							Button {
+								print("Icon \(appIcon) was pressed.")
+								UIApplication.shared.setAlternateIconName(appIcon) { error in
+									if (error != nil) {
+										print("Failed request to update the app's icon: \(String(describing: error?.localizedDescription))")
+										
+									} else {
+										print("Success! You have changed the app's icon to \(appIcon)")
+									}
+								}
+								
+							} label: {
+								
+								Image("\(appIcon)-Preview")
+									.resizable()
+									.scaledToFit()
+									.frame(width: 80, height: 80)
+									.cornerRadius(16)
+							}
+							.buttonStyle(.borderless)
+							
+						}
+					}
+				}
+				.padding(.top, 12)
+				
+				Text("Choose your favourite app icon from the selection above")
+					.frame(minWidth: 0, maxWidth: .infinity)
+					.multilineTextAlignment(.center)
+					.foregroundColor(.secondary)
+					.font(.footnote)
+					.padding(.bottom, 12)
+			}
+			.listRowSeparator(.hidden)
 			
 			// MARK: - section about
 			
@@ -73,7 +130,7 @@ struct SettingsView: View {
 					Text("Copyright © All rights reserved. ")
 					Spacer()
 				}
-			.padding(.vertical, 8)
+					.padding(.vertical, 8)
 			) {
 				CustomListRowView(
 					rowLabel: "Application",
