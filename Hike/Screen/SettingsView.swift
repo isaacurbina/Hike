a452b5ec4df6e334dd435a785f5d7dc0dbfaad13
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-02-19 18:40:10 -0600

    33. 7. SwiftUI List and Labeled Content

--- Hike/Screen/SettingsView.swift
+++ Hike/Screen/SettingsView.swift
@@ -65,6 +65,60 @@ struct SettingsView: View {
 			
 			
 			// MARK: - section about
+			
+			Section(
+				header: Text("ABOUT THE APP"),
+				footer: HStack {
+					Spacer()
+					Text("Copyright © All rights reserved. ")
+					Spacer()
+				}
+			.padding(.vertical, 8)
+			) {
+				CustomListRowView(
+					rowLabel: "Application",
+					rowIcon: "apps.iphone",
+					rowContent: "HIKE",
+					rowTintColor: .blue
+				)
+				CustomListRowView(
+					rowLabel: "Compatibility",
+					rowIcon: "info.circle",
+					rowContent: "iOS, iPadOS",
+					rowTintColor: .red
+				)
+				CustomListRowView(
+					rowLabel: "Technology",
+					rowIcon: "swift",
+					rowContent: "Swift",
+					rowTintColor: .orange
+				)
+				CustomListRowView(
+					rowLabel: "Version",
+					rowIcon: "gear",
+					rowContent: "1.0",
+					rowTintColor: .purple
+				)
+				CustomListRowView(
+					rowLabel: "Developer",
+					rowIcon: "ellipsis.curlybraces",
+					rowContent: "Isaac Urbina",
+					rowTintColor: .mint
+				)
+				CustomListRowView(
+					rowLabel: "Designer",
+					rowIcon: "paintpalette",
+					rowContent: "Robert Petras",
+					rowTintColor: .pink
+				)
+				CustomListRowView(
+					rowLabel: "Website",
+					rowIcon: "globe",
+					rowTintColor: .indigo,
+					rowLinkLabel: "Credo Academy",
+					rowLinkDestination: "https://credo.academy"
+				)
+			}
 		}
 	}
 }
